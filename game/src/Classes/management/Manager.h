4f3067c760bf7cb6a1b08042ba6e6b1efc40c7e3
Kotov Kirill <kotovkirilldev@gmail.com>
2023-05-20 18:42:37 +0300

    Missing scene management flow stuff

---
+++ game/src/Classes/management/Manager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Config.h"
+
+NS_CC_BEGIN
+
+class Manager
+{
+public:
+    virtual ~Manager() = default;
+    virtual void update(float _delta) {}
+    virtual void shutdown() {}
+    
+#ifdef USE_IMGUI
+    virtual void updateImGui() {}
+#endif
+};
+
+NS_CC_END
